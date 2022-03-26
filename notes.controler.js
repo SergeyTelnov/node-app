5b97c11805f86ba3bea059242c36ac323910bf12
Sergey Telnov <st@mail.ru>
2022-03-26 15:02:08 +0300

    create node app

---
+++ notes.controler.js
@@ -0,0 +1,45 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+  notes.push(note);
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgBlue("Note was added!"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.green(note.id), chalk.blue(note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  console.log(chalk.bgRed("Note was removed"));
+  await fs.writeFile(
+    notesPath,
+    JSON.stringify(notes.filter((n) => n.id !== JSON.stringify(id)))
+  );
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
